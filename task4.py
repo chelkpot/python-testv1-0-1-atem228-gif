951453fb6869026692bc2cdb21d348013d856eb0
atem228-gif <atem.228@mail.ru>
2025-11-15 11:56:18 +0500

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,11 @@
+# tasks/task4.py
+
+def solve():
+    # Ниже пишите решение задачи
+    n = int(input().strip())
+    print(n + 2 - (n % 2))
+
+
+# Код ниже не трогать! он нужен для тестов
+if __name__ == "__main__":
+    solve()
